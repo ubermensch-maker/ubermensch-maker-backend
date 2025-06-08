42a0a3d7b000d8b93534438e0f6ad92e87968ecb
jiyoungan <ajy09844@gmail.com>
2025-06-08 19:39:33 +0900

    fix(message): update message content type

--- src/main/java/com/example/todo/message/enums/ContentType.java
+++ src/main/java/com/example/todo/message/enums/ContentType.java
@@ -3,5 +3,5 @@ package com.example.todo.message.enums;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum ContentType {
-    @JsonProperty("input_text") TEXT,
+    @JsonProperty("text") TEXT
 }
